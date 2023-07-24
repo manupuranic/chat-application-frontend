2f8ff023314696e1470f2aa6c237a1cdbcd719a2
manupuranic <manupuranic@gmail.com>
2023-07-24 18:01:59 +0530

    make groups

--- chat.js
+++ chat.js
@@ -3,6 +3,21 @@ const form = document.getElementById("send-message");
 const token = localStorage.getItem("token");
 const profile = document.getElementById("profile");
 const tableBody = document.getElementById("table-body");
+const logout = document.getElementById("logout");
+const newGroup = document.getElementById("newgroup");
+const groupList = document.getElementById("group-list");
+const menuBtn = document.getElementById("menu-btn");
+
+logout.addEventListener("click", () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("currentGpId");
+  localStorage.removeItem("newGroupId");
+  window.location.href = "./login/login.html";
+});
+
+newGroup.addEventListener("click", () => {
+  window.location.href = "./newgroup/new-group.html";
+});
 
 if (!token) {
   window.location.href = "./login/login.html";
@@ -45,54 +60,109 @@ const displayChats = (chat) => {
   tableBody.appendChild(trow);
 };
 
-const getChats = async () => {
-  tableBody.replaceChildren();
-  let localMessages = JSON.parse(localStorage.getItem("messages"));
-  const lastMsgId = localMessages
-    ? localMessages[localMessages.length - 1].id
-    : -1;
+const openGroupChat = (e) => {
+  const gpId = e.target.id;
+  const gpName = e.target.innerText;
+  localStorage.setItem("currentGpId", gpId);
+  localStorage.setItem("currentGpName", gpName);
+  profile.replaceChildren();
+  profile.appendChild(document.createTextNode(gpName));
+  localStorage.setItem("messages", JSON.stringify([]));
+  getChats();
+  menuBtn.click();
+};
+
+const displayGroups = (group) => {
+  const li = document.createElement("li");
+  li.className = "list-group-item users";
+  li.id = group.id;
+  li.appendChild(document.createTextNode(group.name));
+  li.addEventListener("click", openGroupChat);
+  groupList.appendChild(li);
+};
+
+const getGroups = async () => {
   try {
-    const response = await axios.get(`${baseUrl}/chat?lastMsgId=${lastMsgId}`, {
+    const response = await axios.get(`${baseUrl}/groups`, {
       headers: { Authentication: token },
     });
-    const chats = response.data.chats;
-    if (localMessages) {
-      localMessages = [...localMessages, ...chats];
-    } else {
-      localMessages = [...chats];
-    }
-    localMessages.forEach((chat) => {
-      displayChats(chat);
+    const groups = response.data.groups;
+    groups.forEach((group) => {
+      displayGroups(group);
     });
-    while (localMessages.length > 10) {
-      localMessages.shift();
+  } catch (error) {}
+};
+
+const getChats = async () => {
+  tableBody.replaceChildren();
+  const gpId = localStorage.getItem("currentGpId");
+  if (gpId) {
+    let localMessages = JSON.parse(localStorage.getItem("messages"));
+    const lastMsgId = localMessages.length
+      ? localMessages[localMessages.length - 1].id
+      : -1;
+    try {
+      const response = await axios.get(
+        `${baseUrl}/chat?lastMsgId=${lastMsgId}&gpId=${gpId}`,
+        {
+          headers: { Authentication: token },
+        }
+      );
+      const chats = response.data.chats;
+      if (localMessages) {
+        localMessages = [...localMessages, ...chats];
+      } else {
+        localMessages = [...chats];
+      }
+      if (localMessages.length) {
+        localMessages.forEach((chat) => {
+          displayChats(chat);
+        });
+        while (localMessages.length > 10) {
+          localMessages.shift();
+        }
+        localStorage.setItem("messages", JSON.stringify(localMessages));
+      } else {
+        tableBody.innerHTML = `
+    <tr><td><h3 style="text-align: center">No Messages</h3></td></tr>`;
+      }
+    } catch (err) {
+      console.log(err);
     }
-    localStorage.setItem("messages", JSON.stringify(localMessages));
-  } catch (err) {
-    console.log(err);
+  } else {
+    tableBody.innerHTML = `
+    <tr><td><h1 class='heading'>Mchat App</h1></td></tr>
+    <tr><td><h3 style="text-align: center">No group selected</h3></td></tr>`;
   }
 };
 
 const onLoad = () => {
-  const userData = parseJwt(token);
-  profile.appendChild(document.createTextNode(userData.userName));
+  const gpName = localStorage.getItem("currentGpName");
+  profile.replaceChildren();
+  if (gpName) {
+    profile.appendChild(document.createTextNode(gpName));
+  }
   getChats();
+  getGroups();
 };
 
 window.addEventListener("DOMContentLoaded", onLoad);
 
-setInterval(getChats, 1000);
-
 const submitHandler = async (e) => {
   e.preventDefault();
+  const gpId = localStorage.getItem("currentGpId");
   const msg = e.target.message;
   const postMessage = {
     msg: msg.value,
   };
   try {
-    const response = await axios.post(`${baseUrl}/chat`, postMessage, {
-      headers: { Authentication: token },
-    });
+    const response = await axios.post(
+      `${baseUrl}/chat?gpId=${gpId}`,
+      postMessage,
+      {
+        headers: { Authentication: token },
+      }
+    );
     console.log(response.data);
     msg.value = "";
   } catch (err) {
@@ -101,3 +171,5 @@ const submitHandler = async (e) => {
 };
 
 form.addEventListener("submit", submitHandler);
+
+setInterval(getChats, 1000);
